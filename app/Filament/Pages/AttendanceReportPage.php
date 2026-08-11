6f4a18e38625f321e70c29e1eb41f17e1940adcd
DosmanGianyar <tailscaledosman@gmail.com>
2026-08-11 19:36:54 +0800

    Update admin sidebar label to Rekap & Laporan Presensi for clarity

--- app/Filament/Pages/AttendanceReportPage.php
+++ app/Filament/Pages/AttendanceReportPage.php
@@ -13,8 +13,8 @@ class AttendanceReportPage extends Page
 {
     protected static string|\BackedEnum|null $navigationIcon       = 'heroicon-o-clipboard-document-list';
     protected static string|\UnitEnum|null   $navigationGroup      = 'Presensi Siswa';
-    protected static ?string                 $navigationLabel      = 'Laporan Presensi';
-    protected static ?string                 $title                = 'Laporan Presensi Bulanan';
+    protected static ?string                 $navigationLabel      = 'Rekap & Laporan Presensi';
+    protected static ?string                 $title                = 'Rekapitulasi & Cetak Laporan Presensi Siswa Bulanan';
     protected static ?int                    $navigationSort       = 15;
 
     public static function canAccess(): bool { return AdminAccess::can('Presensi Siswa'); }
