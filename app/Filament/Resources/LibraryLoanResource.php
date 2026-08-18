75f708fc1a9cb9d5d2a85d4ce8920c275acd7066
DosmanGianyar <tailscaledosmn@gmail.com>
2026-08-18 09:44:21 +0800

    fix(filament): update Action import namespace in LibraryLoanResource.php

--- app/Filament/Resources/LibraryLoanResource.php
+++ app/Filament/Resources/LibraryLoanResource.php
@@ -14,7 +14,7 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Filament\Tables;
-use Filament\Tables\Actions\Action;
+use Filament\Actions\Action;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
