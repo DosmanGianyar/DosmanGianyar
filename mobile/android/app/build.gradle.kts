d25c92b1351c7641e57bc40343ad9f331bb02475
DosmanGianyar <tailscaledosmn@gmail.com>
2026-08-20 12:52:45 +0800

    fix: set minSdk to 23 in Android build.gradle.kts

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sman1gianyar.sims_mobile"
-        minSdk = flutter.minSdkVersion
+        minSdk        = 23
         targetSdk     = 36
         versionCode   = flutter.versionCode
         versionName   = flutter.versionName
